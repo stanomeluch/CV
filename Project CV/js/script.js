6380d4f3a3dae7c88900d9618cca35209421a26a
Stanislav Meluch <64906706+stanomeluch@users.noreply.github.com>
2021-02-16 20:18:27 +0100

    Add files via upload

---
+++ Project CV/js/script.js
@@ -0,0 +1,31 @@
+/*To to Top Button  */
+
+let btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
+
+/* Hide Me function*/
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".hideMe").each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 400);
+      }
+    });
+  });
+});
